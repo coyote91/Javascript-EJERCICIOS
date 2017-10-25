47be4549f265897925d0db3ae7c5bc17e56c4336
coyote91 <david.f.r91@hotmail.com>
2017-10-24 21:54:25 -0500

    Añado ejercicio1 de objeto que tiene adentro un metodo o funcion cuyo nombre lleva dos puntos y como llamarla

---
+++ objetosconmetodos/objetosconmetodos.php
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+
+var obj = {
+    value: 10,
+
+}
+
+console.log(obj.value);
+
+// guia 1    https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Trabajando_con_objectos
+
+// guia 2    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Unnamed_function_statement
+
+var greeter = {
+  german: function () {
+     alert("Moin");
+  }
+};
+
+greeter.german();
+
+</script>
