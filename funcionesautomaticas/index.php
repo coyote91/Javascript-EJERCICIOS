523536802e6b1c3b290d535e4e0b7347a713698b
coyote91 <david.f.r91@hotmail.com>
2018-04-20 15:45:37 -0500

    añado manual de funciones anonimas

--- funcionesautomaticas/index.php
+++ funcionesautomaticas/index.php
@@ -6,9 +6,16 @@
   </head>
   <body>
 
+<!--
+
+ esta pagina cuenta con excelentes tutoriales de javascript
+
+ funciones anonimas:  tutorial   http://www.brenkoweb.com/tutorials/javascript/js-anonymous-functions/introduction-to-anonymous-functions
+
+-->
 
 
     <script src="./funcionesautomaticas.js" charset="utf-8"></script>
-    
+
   </body>
 </html>
